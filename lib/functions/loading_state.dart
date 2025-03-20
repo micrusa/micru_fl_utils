949d77a9f58df950c750d3344ef47c7114451849
Miguel Cruces <micrusa@hotmail.es>
2025-03-20 21:18:05 +0100

    Fix loading state mixin

--- lib/functions/loading_state.dart
+++ lib/functions/loading_state.dart
@@ -4,58 +4,56 @@ import 'package:flutter/widgets.dart';
 /// The loading state is unique to the function instance, so you can have multiple
 /// functions (or buttons) with their own loading state.
 mixin LoadingStateMixin<T extends StatefulWidget> on State<T> {
-  final Map<int, ValueNotifier<bool>> _loadingStates = {};
+  Map<int, bool> _loadingStates = {};
+
+  void _setStateOf(int key, bool value) {
+    setState(() {
+      _loadingStates = {
+        ..._loadingStates,
+        key: value,
+      };
+    });
+  }
 
   /// Returns a function that wraps the given function with loading state.
   /// The loading state is unique to the function instance.
-  Future<void> withLoading(Future<void> Function() function) async {
-    final loadingState = getLoadingState(function);
-    if (loadingState.value) {
-      return;
+  Future<K> Function()? withLoading<K>(Future<K> Function() function) {
+    final loadingState = isLoading(function);
+    if (loadingState) {
+      return null;
     }
 
-    loadingState.value = true;
-    try {
-      return await function();
-    } finally {
-      loadingState.value = false;
-    }
+    return () async {
+      _setStateOf(function.hashCode, true);
+      final result = await function();
+      _setStateOf(function.hashCode, false);
+      return result;
+    };
   }
 
   /// Returns a function that wraps the given function with a global loading state.
   /// If any of the loading states are `true`, the function will not be executed.
   /// Useful if you want to prevent multiple functions firing at the same time.
-  Future<void> withGlobalLoading(Future<void> Function() function) async {
+  Future<K> Function()? withGlobalLoading<K>(Future<K> Function() function) {
     if (isAnyLoading()) {
-      return;
+      return null;
     }
 
-    final localLoadingState = getLoadingState(function);
-    localLoadingState.value = true;
-    try {
-      return await function();
-    } finally {
-      localLoadingState.value = false;
-    }
+    return () async {
+      _setStateOf(function.hashCode, true);
+      final result = await function();
+      _setStateOf(function.hashCode, false);
+      return result;
+    };
   }
 
   /// Returns `true` if any of the loading states are `true`.
   bool isAnyLoading() {
-    return _loadingStates.values.any((state) => state.value);
+    return _loadingStates.values.any((state) => state);
   }
 
   /// Gets the loading state for a specific function
-  ValueNotifier<bool> getLoadingState(Future<void> Function() function) {
-    return _loadingStates.putIfAbsent(
-        function.hashCode, () => ValueNotifier(false));
-  }
-
-  @override
-  void dispose() {
-    for (final state in _loadingStates.values) {
-      state.dispose();
-    }
-    _loadingStates.clear();
-    super.dispose();
+  bool isLoading(Future<void> Function() function) {
+    return _loadingStates.putIfAbsent(function.hashCode, () => false);
   }
 }
